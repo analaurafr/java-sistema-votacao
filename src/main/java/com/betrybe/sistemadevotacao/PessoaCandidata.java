0f0d648f6b1190a8c8ae745c783943f200171852
analaurafr <laurafernandes@outlook.com>
2024-04-17 20:38:17 -0300

    req 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,30 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
 
+  public PessoaCandidata(String nome, int numero) {
+    super();
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    this.votos++;
+  }
 }
