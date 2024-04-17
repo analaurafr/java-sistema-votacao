b5eff441aa5d768c2e5ef1a239c1fbe3670f4cb0
analaurafr <laurafernandes@outlook.com>
2024-04-17 20:48:09 -0300

    req 3

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,24 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa eleitora.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa{
+  private String cpf;
 
+  /**
+   * Gets cpf.
+   *
+   * @return the cpf
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Sets cpf.
+   *
+   * @param cpf the cpf
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
