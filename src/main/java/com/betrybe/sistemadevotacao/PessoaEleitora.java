995760f23cda73f6e444cc40013df25ed0c7afd6
analaurafr <laurafernandes@outlook.com>
2024-04-17 20:51:42 -0300

    corrige espaço

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,7 +3,7 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa eleitora.
  */
-public class PessoaEleitora extends Pessoa{
+public class PessoaEleitora extends Pessoa {
   private String cpf;
 
   /**
