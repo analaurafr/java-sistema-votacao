3ebb825df09674c379e90d8c9804c58b7796d88d
analaurafr <laurafernandes@outlook.com>
2024-04-17 21:19:41 -0300

    req 4

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,42 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
+
 /**
  * The type Gerenciamento votacao.
  */
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  /**
+   * Instantiates a new Gerenciamento votacao.
+   */
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<>();
+    this.pessoasEleitoras = new ArrayList<>();
+    this.cpfsComputados = new ArrayList<>();
+  }
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
